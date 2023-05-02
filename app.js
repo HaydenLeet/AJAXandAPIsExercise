c45eb05df26095dd19f439188f346436330bd821
Your Name Here <your_email@youremail.com>
2023-05-02 12:42:07 -0400

    Fixed repo

---
+++ app.js
@@ -0,0 +1,102 @@
+console.log(`Sup Globe Peeps?`);
+
+// AJAX and APIs Exercise
+
+// 1
+const first = document.querySelector('#first');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+first.append(p1);
+first.append(p2);
+const oneAJSON = `{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline":"Because he didn't get arrays."}`;
+// 1
+// const oneJSON = `{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline",:"Because he didn't get arrays."}`;
+jokeJS1 = JSON.parse(oneAJSON);
+// 1b
+p1.innerHTML = jokeJS1.setup
+// 1c
+p2.innerHTML = jokeJS1.punchline
+
+
+// 2
+const second = document.querySelector('#second');
+const p3 = document.createElement('p');
+const p4 = document.createElement('p');
+second.append(p3);
+second.append(p4);
+// 2a
+const activity = axios.get('https://www.boredapi.com/api/activity')
+// 2b
+.then((result) => {
+    activityjs2 = result.data
+    p3.innerText = activityjs2.type
+    p4.innerText = activityjs2.activity
+    console.log('Question 2 was successful');
+})
+.catch((err) => {
+    console.log('Question 2 failed to GET ');
+    console.log(err);
+})
+
+
+// 3
+const third = document.querySelector('#third');
+const p5 = document.createElement('p');
+const p6 = document.createElement('p');
+third.append(p5);
+third.append(p6);
+// 3b
+async function activityFunc() {
+    try {
+        // 3a
+        const activityjs3 = await axios.get('https://www.boredapi.com/api/activity')
+        // 3c
+        p5.innerText = activityjs3.data.activity
+        p6.innerText = activityjs3.data.type
+    } catch (err) {
+        console.log('Question 3 failed to get');
+        console.log(err);
+    }
+}
+activityFunc();
+
+// 4
+const fourth = document.querySelector('#fourth');
+const p7 = document.createElement('p');
+fourth.append(p7);
+// 4a
+// https://www.tvmaze.com.api
+async function tvMazeFunc(){
+    try {
+        let theShow = await axios.get('https://api.tvmaze.com/singlesearch/shows?q=Mandalorian&embed=episodes')
+        console.log(theShow);
+        theEpisode = theShow.data._embedded.episodes;
+        console.log(theEpisode[15].name);
+        // data._embedded.episodes
+        p7.innerText = theEpisode[15].name;
+        console.log('Q4: Success');
+    } catch (err) {
+        console.log(err);
+        console.log('Q4: Error');
+    }
+}
+
+tvMazeFunc();
+
+// Bonus
+// 5
+const img = document.createElement('img')
+const body = document.querySelector('body')
+
+async function pika() {
+    try {
+        const pikaPic = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+        img.src = pikaPic.data.sprites.front_shiny
+        body.append(img)
+        console.log('Bonus question was successful');
+    } catch (err) {
+        console.log('Bonus question failed to get');
+        console.log(err);
+    }
+}
+pika()
